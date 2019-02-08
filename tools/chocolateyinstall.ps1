03aa8bd576f14aa064a21c56d5b5059c154366f0
chemputer <bdt1995@gmail.com>
2019-02-08 17:20:35 -0500

    chocoinstall, chocouninstall, license, all updated
    filled out the install and uninstall ps1 files as well as the nuspec with all the details

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/nspire-emus/firebird/releases/download/v1.4/firebird-emu-windows.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'firebird-emu*'
+
+  checksum      = 'DB8A33933BE5E9327277DB8F949C98D6ED58B936B1FE07D13B67FABA9B47732F'
+  checksumType  = 'sha256'
+
+}
+
+Install-ChocolateyZipPackage @packageArgs
